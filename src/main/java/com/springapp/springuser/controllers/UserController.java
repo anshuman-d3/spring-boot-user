43bfd06f644bb58cc3b40c3fd1efc1afc0bb0170
Anshuman <anshuman.c6@hotmail.com>
2019-03-06 16:34:34 +0530

    - Register Controller Modified to return ResponseEntity. - Email Template Image Sources Changed.

--- src/main/java/com/springapp/springuser/controllers/UserController.java
+++ src/main/java/com/springapp/springuser/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.springapp.springuser.controllers;
 
 import com.springapp.springuser.models.ApplicationUser;
 import com.springapp.springuser.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,14 +22,15 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public ApplicationUser signUp(@RequestBody ApplicationUser applicationUser) {
+    public ResponseEntity<ApplicationUser> signUp(@RequestBody ApplicationUser applicationUser) {
         System.out.println("sign-up post method");
         applicationUser.setPassword(bCryptPasswordEncoder.encode(applicationUser.getPassword()));
         try{
-            return userRepository.save(applicationUser);
+            userRepository.save(applicationUser);
+            return new ResponseEntity<ApplicationUser>(applicationUser, HttpStatus.CREATED);
         }
         catch (Exception e){
-            return null;
+            return new ResponseEntity<ApplicationUser>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 //        System.out.println("saving applicationUser object successful");
     }
