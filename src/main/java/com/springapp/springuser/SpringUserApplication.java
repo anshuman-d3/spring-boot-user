f9c10d80e47077ff6c69b45b7ae1f07607923937
Anshuman <anshuman.c6@hotmail.com>
2019-03-11 19:10:28 +0530

    Fix for Eclipse STS.

--- src/main/java/com/springapp/springuser/SpringUserApplication.java
+++ src/main/java/com/springapp/springuser/SpringUserApplication.java
@@ -1,13 +1,16 @@
 package com.springapp.springuser;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class SpringUserApplication {
 
     @Bean
--- src/main/java/com/springapp/springuser/models/ApplicationUser.java
+++ src/main/java/com/springapp/springuser/models/ApplicationUser.java
@@ -30,4 +30,52 @@ public class ApplicationUser {
     @ManyToMany
     @JsonIgnore
     private Set<UserRole> userRoles;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
 }
